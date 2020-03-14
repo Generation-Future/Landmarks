e7d84bb9b6c978f160a61bd75141b4e9994a1e27
Marc Haldenwang <marc.haldenwang@outlook.de>
2020-03-14 13:21:20 +0100

    Initial Commit

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Marc Haldenwang on 14.03.20.
+//  Copyright © 2020 Marc Haldenwang. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
